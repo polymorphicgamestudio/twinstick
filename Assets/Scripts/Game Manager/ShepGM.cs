abde0abe3074dfb923f32d57adcb7ff8a324b3d6
justin.riggs101@gmail.com <>
2023-02-13 16:18:46 -0800

    Updated structure to make it cleaner

---
+++ Assets/Scripts/Game Manager/ShepGM.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**************************************************
+ This holds references to things for easy retrieval
+things add and remove themselves in awake / destroy
+**************************************************/
+public class ShepGM : MonoBehaviour
+	{
+
+	public static Transform player;
+	static List<Transform>[] things = new List<Transform>[(int)Thing.Count];
+
+	public enum Thing {
+		Slime,
+		Turret,
+		SlimeProjectile,
+		TurretProjectile,
+		PlayerProjectile,
+		Count
+	}
+	
+	static ShepGM() {
+		for (int q = 0; q < things.Length;q++) {
+			things[q] = new List<Transform>();
+		}
+	}
+	
+	public static List<Transform> GetList(Thing thing) {
+		return things[(int)thing];
+	}
+
+	public static int GetCount(Thing thing) {
+		return GetList(thing).Count;
+	}
+
+	public static Transform GetRandom(Thing thing) {
+		return GetList(thing)[Random.Range(0, GetCount(thing))];
+	}
+
+	public static Transform GetNearest(Thing thing, Vector3 point) {
+		if (GetCount(thing) > 0) return GetNearestFromList(GetList(thing), point);
+		return null;
+	}
+	
+	public static Transform GetNearestFromList(List<Transform> list, Vector3 point) {
+		float nearestDistance = float.MaxValue;
+		Transform nearestObject = null;
+		for (int i = 0; i < list.Count; i++) {
+			float distance = Vector3.Distance(list[i].transform.position, point);
+			if (distance < nearestDistance) {
+				nearestDistance = distance;
+				nearestObject = list[i];
+			}
+		}
+		return nearestObject;
+	}
+}
