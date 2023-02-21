e8e054a2e3e17b4ce6c8faf28bb3e4d77e4b7ea8
brobison@uidaho.edu <>
2023-02-21 12:00:43 -0800

    More Effects!   All the turrets have at least a projectile or line renderer now. (In Main Scene)  hit effects, sounds, animations, etc will come later.

--- Assets/Scripts/Bullets/Laser.cs
+++ Assets/Scripts/Bullets/Laser.cs
@@ -8,15 +8,18 @@ public class Laser : MonoBehaviour {
 	public Transform muzzleEffect;
     LineRenderer beam;
 
+    public bool particles = true;
+
     void Start () {
         beam = GetComponent<LineRenderer>();
     }
     void Update() {
         Vector3 dir = laserEnd.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(dir);
-        laserEnd.rotation = rotation;
-        muzzleEffect.rotation = rotation;
-
+        if (particles) {
+            Quaternion rotation = Quaternion.LookRotation(dir);
+            laserEnd.rotation = rotation;
+            muzzleEffect.rotation = rotation;
+        }
         beam.SetPosition(0, transform.position);
         beam.SetPosition(1, laserEnd.position);
     }
