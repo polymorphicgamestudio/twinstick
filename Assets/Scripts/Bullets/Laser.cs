253d3e22c9c6d6255dd498e59a8331fc0d518a6a
brobison@uidaho.edu <>
2023-02-21 09:59:07 -0800

    effects

--- Assets/Scripts/Bullets/Laser.cs
+++ Assets/Scripts/Bullets/Laser.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Laser : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Laser : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+	public Transform laserEnd;
+	public Transform muzzleEffect;
+    LineRenderer beam;
+
+    void Start () {
+        beam = GetComponent<LineRenderer>();
+    }
+    void Update() {
+        Vector3 dir = laserEnd.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+        laserEnd.rotation = rotation;
+        muzzleEffect.rotation = rotation;
+
+        beam.SetPosition(0, transform.position);
+        beam.SetPosition(1, laserEnd.position);
     }
 }
