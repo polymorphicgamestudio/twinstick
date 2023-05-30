63b765d57035c2917c76e51057df2acaf6598575
brobison@uidaho.edu <>
2023-05-30 10:53:14 -0700

    partially working new HUD in my scene

--- Assets/Scripts/UI/HUDActionSelectionBar.cs
+++ Assets/Scripts/UI/HUDActionSelectionBar.cs
@@ -5,8 +5,8 @@ public class HUDActionSelectionBar : MonoBehaviour {
 
 	[SerializeField] TowerPlacement placement; // on playermodel
 	[SerializeField] RectTransform selectionIndicator;
-	Vector2 positionRange = new Vector2(-652.5f, 652.5f);
-	float positionGoal, positionCurrent = -652.5f;
+	Vector2 positionRange = new Vector2(-720f, 548.6f);
+	float positionGoal, positionCurrent = -720f;
 	float lerpFraction = 0f;
 
 
@@ -17,6 +17,9 @@ public class HUDActionSelectionBar : MonoBehaviour {
 		lerpFraction = (placement.actionSelectionNumber - 1) / 9f;
 		positionGoal = Mathf.Lerp(positionRange.x, positionRange.y, lerpFraction);
 		positionCurrent = (9 * positionCurrent + positionGoal) / 10f;
-		selectionIndicator.anchoredPosition = new Vector2(positionCurrent, 0);
+		selectionIndicator.anchoredPosition = new Vector2(positionCurrent, 9);
+	}
+	public void SetSelection(int selectionNumber) {
+		placement.actionSelectionNumber = selectionNumber;
 	}
 }
